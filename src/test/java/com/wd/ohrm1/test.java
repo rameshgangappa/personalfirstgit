dc0d56ca9e4d7b35797274ed743a7ef375b0404f
Dell <Dell@Dell-PC>
2017-09-19 14:19:26 +0530

    1st update

---
+++ src/test/java/com/wd/ohrm1/test.java
@@ -0,0 +1,21 @@
+package com.wd.ohrm1;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class test {
+
+	@Test
+public void test1() {
+	System.out.println("this is begginingof test");
+	
+}
+
+	@Test(dependsOnMethods="test1")
+public void test2() {
+	System.out.println("end of test");
+	
+}
+
+}
